3cabea8c7d1f0e8debe2bd15c1cb7261960698b0
gfiori <gino.fiori@hotmail.com>
2015-09-18 20:20:11 -0300

    Add ctrlAjedrez

---
+++ TpAjedrez/src/cpLogica/CtrlAjedrez.java
@@ -0,0 +1,43 @@
+package cpLogica;
+
+import cpDatos.*;
+import cpData.*;
+
+import java.sql.*;
+
+import cpPresentacion.pntAjedrez;
+
+public class CtrlAjedrez {
+	
+	Partida p      = new Partida();
+	DataPartida dp = new DataPartida();
+	
+	public void nuevaPartida(long dniBlancas, String nombreBlancas, String apellBlancas, long dniNegras, String nombreNegras, String apellNegras){
+		
+		int idPartida;
+		
+		idPartida = dp.recuperarPartida(dniBlancas, dniNegras);
+		
+		if (idPartida==0){
+			p.inicializar("BLANCO");
+			p.inicializar("NEGRO");
+			p.cargarJugadores(dniBlancas, nombreBlancas, apellBlancas, dniNegras, nombreNegras, apellNegras);
+		}else{
+			//
+		}
+		
+	}
+
+	public void moverPiezas(String origen, String destino){
+		p.moverPiezas(origen, destino);
+	}
+	
+	public Jugador devolverTurno(){
+		return p.devolverTurno();
+	}
+
+	public String recuperarFicha(String posicion){
+		return p.recuperarFicha(posicion);
+	};
+
+}
